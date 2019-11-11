52a693086c18b46c33c0aa3794af1bdb0913459f
cedorman <clark.dorman@nextcentury.com>
2019-11-11 05:30:50 -0500

    WIP writing out status.

--- occluder/occ_view.py
+++ occluder/occ_view.py
@@ -5,7 +5,7 @@
 import json
 import os
 import shutil
-from PIL import Image
+from PIL import Image, ImageDraw
 from pathlib import Path
 import random
 from statistics import mode
@@ -36,7 +36,7 @@ class Obj:
             self.miny = y
         if y > self.maxy:
             self.maxy = y
-        diffy =  abs(self.maxy - self.miny)
+        diffy = abs(self.maxy - self.miny)
         if diffy > 0:
             self.aspect_ratio = abs(self.maxx - self.minx) / diffy
         else:
@@ -45,7 +45,7 @@ class Obj:
     def __str__(self):
         return "(" + str(self.color) + " [ " + str(self.minx) + ", " + str(self.maxx) + ", " + str(
             self.miny) + ", " + str(self.maxy) + " ] " + " ct: " + str(
-            self.pixel_count) + " AR: " + str(self.aspect_ratio)\
+            self.pixel_count) + " AR: " + str(self.aspect_ratio) \
                + ")"
 
 
@@ -62,7 +62,7 @@ class MaskInfo:
     def get_num_obj(self):
         return len(self.objects)
 
-    def get_obj(self, obj_num):
+    def get_obj(self):
         return self.objects
 
     def get_objects_for_frame(self, in_path, frame_num):
@@ -97,12 +97,12 @@ class MaskInfo:
                 to_be_removed.append(key)
 
             # too small, must be non-occluder object
-            elif val.pixel_count < 850:
+            elif val.pixel_count < 800:
                 to_be_removed.append(key)
 
             # aspect ratio wrong
-            elif val.pixel_count > 800 and val.pixel_count < 1200:
-                if val.aspect_ratio > 0.8 and val.aspect_ratio < 1.2:
+            elif 800 < val.pixel_count < 1400:
+                if 0.8 < val.aspect_ratio < 1.5:
                     to_be_removed.append(key)
 
         for x in to_be_removed:
@@ -114,37 +114,67 @@ class OccluderViewer:
     def __init__(self):
         self.test_num = 1
         self.dataDir = Path("/mnt/ssd/cdorman/data/mcs/intphys/test/O1")
-
-    def read_new_scene(self):
-        print("Reading in new test {}".format(self.test_num))
-
-        current_dir_path = self.dataDir / self.test_num_string
-        # self.process_mask(current_dir_path, 50)
-        self.frame_slider.set_val(50)
+        self.masks = []
 
     def process_mask(self, mask_path, frame_num):
-        init_masks = []
+        self.masks.clear()
         for scene in range(4):
-            init_masks.append(MaskInfo(mask_path / str(scene + 1), frame_num))
+            self.masks.append(MaskInfo(mask_path / str(scene + 1), frame_num))
 
     def update_slider(self, val):
+        # Change the frame
         frame_num = int(self.frame_slider.val)
         self.process_mask(self.dataDir / self.test_num_string, frame_num)
-        print(" Freq {}".format(frame_num))
-        frame_num_string = str(frame_num).zfill(3)
 
+        # Draw the images
+        frame_num_string = str(frame_num).zfill(3)
         for ii in range(0, 4):
             image_name = self.dataDir / self.test_num_string / str(ii + 1) / "scene" / (
                     "scene_" + frame_num_string + ".png")
-            img_src = mpimg.imread(str(image_name))
+
+            img_src = Image.open(image_name)
+            # img_src = mpimg.imread(str(image_name))
+            draw = ImageDraw.Draw(img_src)
+            for obj in self.masks[ii].get_obj().values():
+                draw.rectangle([(obj.minx, obj.miny), (obj.maxx, obj.maxy)], width=2)
+
             self.axs[ii].imshow(img_src)
 
         self.fig.canvas.draw_idle()
 
+    def write_out_status_for_scene(self, scene_num):
+
+        status_json = {}
+        frames = []
+        header = {}
+        statu
+        # Make sure there are the same number of occluders in the scene over all frames
+        num = -1
+        for frame_num in range(1,101):
+            mask= MaskInfo(self.dataDir / self.test_num_string / str(scene_num), frame_num)
+            obj = mask.get_obj
+            if num == -1:
+                num = len(obj)
+            elif len(obj) != num:
+                print("Problem in test {} scene {} frame {}. Wrong num ".format(self.test_num, scene_num, frame_num))
+
+
+
+        status_path = self.dataDir / self.test_num_string / str(scene_num) / "status.json"
+        with status_path.open("w") as outfile:
+
+
+
+    def write_out_status(self):
+        for ii in range(0, 4):
+            self.write_out_status_for_scene(ii+1)
+
     def update_keypress(self, event):
 
         sys.stdout.flush()
-        print("event.key {}".format(event.key))
+
+        if event.key == 'x':
+            self.write_out_status()
 
         if event.key == 'right':
             self.test_num = self.test_num + 1
@@ -153,8 +183,8 @@ class OccluderViewer:
         self.test_num_string = str(self.test_num).zfill(4)
         plt.title(self.test_num_string)
 
-        self.read_new_scene()
-        self.update_slider(1)
+        print("Reading in new test {}".format(self.test_num))
+        self.frame_slider.set_val(50)
 
     def set_up_view(self, test_num):
 
