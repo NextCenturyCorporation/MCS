4ff977e25806928c3f5e8e8e18e2c687d4e59b75
cedorman <clark.dorman@nextcentury.com>
2019-11-20 16:08:32 -0500

    Moved to own directory

--- validation/mcs_validate_test.py
+++ validation/mcs_validate_test.py
@@ -11,45 +11,45 @@ class MyTestCase(unittest.TestCase):
     Note that since this is using unittest, that the working directory is a subdirectory that
     this file exists in, so you have to go up a directory to find the submissions
     """
-
+    
     def test_voe(self):
         v = MCSEval1Validator()
-        output = v.parse_voe(Path("../submissions/voe_O3_0301_4.txt"))
+        output = v.parse_voe(Path("submissions/voe_O3_0301_4.txt"))
         self.assertEqual(output, True)
 
     def test_voe(self):
         v = MCSEval1Validator()
-        output = v.parse_voe(Path("../submissions/voe_wrong_num.txt"))
+        output = v.parse_voe(Path("submissions/voe_wrong_num.txt"))
         self.assertEqual(output, False)
 
     def test_location(self):
         v = MCSEval1Validator()
-        output = v.parse_location(Path("../submissions/location.txt"))
+        output = v.parse_location(Path("submissions/location.txt"))
         self.assertEqual(output, True)
 
     def test_location_mask_out_of_range(self):
         v = MCSEval1Validator()
-        output = v.parse_location(Path("../submissions/location_mask_out_of_range.txt"))
+        output = v.parse_location(Path("submissions/location_mask_out_of_range.txt"))
         self.assertEqual(output, False)
 
     def test_read_description(self):
         v = MCSEval1Validator()
-        output = v.parse_description(Path("../submissions/description.json"))
+        output = v.parse_description(Path("submissions/description.json"))
         self.assertEqual(output, True)
 
     def test_description_no_performer(self):
         v = MCSEval1Validator()
-        output = v.parse_description(Path("../submissions/description_no_performer.json"))
+        output = v.parse_description(Path("submissions/description_no_performer.json"))
         self.assertEqual(output, False)
 
     def test_description_bad_json(self):
         v = MCSEval1Validator()
-        output = v.parse_description(Path("../submissions/description_bad_json.json"))
+        output = v.parse_description(Path("submissions/description_bad_json.json"))
         self.assertEqual(output, False)
 
     def test_nosuchfile(self):
         v = MCSEval1Validator()
-        output = v.validate("../submissions/blah")
+        output = v.validate("submissions/blah")
         self.assertEqual(output, False)
 
     def test_emptyfile(self):
@@ -59,17 +59,17 @@ class MyTestCase(unittest.TestCase):
 
     def test_doesntendwithzip(self):
         v = MCSEval1Validator()
-        output = v.validate("../submissions/submission_invalid_not_zip_ending")
+        output = v.validate("submissions/submission_invalid_not_zip_ending")
         self.assertEqual(output, False)
 
     def test_invalidzip(self):
         v = MCSEval1Validator()
-        output = v.validate("../submissions/submission_bad_zip.zip")
+        output = v.validate("submissions/submission_bad_zip.zip")
         self.assertEqual(output, False)
 
     def test_passes(self):
         v = MCSEval1Validator()
-        output = v.validate("../submissions/submission_valid.zip")
+        output = v.validate("submissions/submission_valid.zip")
         self.assertEqual(output, True)
 
 
