c592ee978cfc23c2c71df94fd8c510f7cc926a81
bwagner <bwagner@pc-2152-bw.hq.nextcentury.com>
2020-03-30 09:41:46 -0400

    Adding optimal path conversion stuff

---
+++ scene_generator/optimal_path.py
@@ -0,0 +1,23 @@
+from extremitypathfinder.extremitypathfinder import PolygonEnvironment as Environment
+
+from scene_generator import *
+
+
+
+# objecs passed because I forgot to record the dimensions :P
+# This is the source for extremitypathfinder: https://github.com/MrMinimal64/extremitypathfinder
+def generatepath(body, objects):
+    '''Boundary has to be CCW, Holes CW'''
+    boundary_coordinates = [ (MAX_SCENE_POSITION, MAX_SCENE_POSITION),(MIN_SCENE_POSITION, MAX_SCENE_POSITION),(MIN_SCENE_POSITION, MIN_SCENE_POSITION), (MAX_SCENE_POSITION,MIN_SCENE_POSITION)]
+    
+    holes= []
+    for object in body['objects']:
+        #Handle walls first
+        if object['type'] == 'cube':
+            dimensions = object['shows'][0]['scale']
+            position = object['shows'][0]['position']
+            rotation = object['shows'][0]['rotation']
+            points = calc_obj_coords(position['x'],position['z'], dimensions['x'],dimensions['z'],rotation['y'])
+            holes.append([(point['x'],point['z']) for point in points])
+                
+    
