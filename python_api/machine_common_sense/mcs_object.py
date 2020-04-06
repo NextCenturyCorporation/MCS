3ba34d2e5247e6f51b4df4fed5ca52741ee54d7d
Dean Wetherby <dean.wetherby@gmail.com>
2020-04-06 15:49:03 -0400

    Fixed typo in documentation

--- python_api/machine_common_sense/mcs_object.py
+++ python_api/machine_common_sense/mcs_object.py
@@ -16,7 +16,7 @@ class MCS_Object(object):
         The normalized direction vector of "x", "y", and "z" degrees between your position and this object's.
         Use "x" and "y" as "rotation" and "horizon" params (respectively) in a "RotateLook" action to face this object.
     distance : float
-        The distance along the 2-dimensional X/Z grid from you oto this object in number of steps ("Move" actions).
+        The distance along the 2-dimensional X/Z grid from you to this object in number of steps ("Move" actions).
     held : boolean
         Whether you are holding this object.
     mass : float
