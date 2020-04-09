5c8bfd60f116caceab1876331d6c8a33954ee4ed
Chris Long <chris.long@nextcentury.com>
2020-04-09 17:12:51 -0400

    RetrievalGoal seems to work

--- scene_generator/goals.py
+++ scene_generator/goals.py
@@ -4,8 +4,48 @@
 
 import copy
 import random
+import uuid
 from abc import ABC, abstractmethod
 from enum import Enum
+
+from geometry import random_position, random_rotation, calc_obj_pos
+
+MAX_OBJECTS = 5
+
+
+def instantiate_object(object_def, object_location):
+    new_object = {
+        'id': str(uuid.uuid4()),
+        'type': object_def['type'],
+        'info': object_def['info'],
+        'mass': object_def['mass']
+    }
+    for attribute in object_def['attributes']:
+        new_object[attribute] = True
+
+    shows = [object_location]
+    new_object['shows'] = shows
+    object_location['stepBegin'] = 0
+    object_location['scale'] = object_def['scale']
+    if 'salientMaterials' in object_def:
+        salientMaterialsIndex = object_def['salientMaterials'][0].upper() + '_MATERIALS'
+        salientMaterial = random.choice(globals().get(salientMaterialsIndex, None))
+        new_object['material'] = salientMaterial
+        new_object['info'].append(object_def['salientMaterials'][0])
+        new_object['salientMaterials'] = object_def['salientMaterials']
+    return new_object
+
+
+def add_objects(object_defs, object_list, rectangles, performer_position):
+    """Add random objects to fill object_list to some random number of objects up to MAX_OBJECTS. If object_list
+    already has more than this randomly determined number, no new objects are added."""
+    object_count = random.randint(1, MAX_OBJECTS)
+    for i in range(len(object_list), object_count):
+        object_def = copy.deepcopy(random.choice(object_defs))
+        obj_location = calc_obj_pos(performer_position, rectangles, object_def)
+        if obj_location is not None:
+            obj = instantiate_object(object_def, obj_location)
+            object_list.append(obj)
 
 
 class AttributeConstraint:
@@ -20,16 +60,45 @@ class AttributeConstraint:
         return self.attribute in obj and self.predicate(obj[self.attribute], *self.arguments)
 
 
+class GoalException(Exception):
+    def __init__(self, message=''):
+        super(GoalException, self).__init__(message)
+
+
 class Goal(ABC):
     """An abstract Goal."""
 
-    @abstractmethod
-    def get_object_constraint_lists(self):
-        return []
+    def __init__(self):
+        self._performer_start = None
+
+    def update_body(self, object_defs, body):
+        body['performerStart'] = self.compute_performer_start()
+        goal_objects, all_objects = self.compute_objects(object_defs)
+        body['objects'] = all_objects
+        body['goal'] = self.get_config(goal_objects)
+        return body
+
+    def compute_performer_start(self):
+        if self._performer_start is None:
+            self._performer_start = {
+                'position': {
+                    'x': random_position(),
+                    'y': 0,
+                    'z': random_position()
+                },
+                'rotation': {
+                    'y': random_rotation()
+                }
+            }
+        return self._performer_start
 
     @abstractmethod
-    def get_config(self, objects):
-        return {}
+    def compute_objects(self, object_defs):
+        pass
+
+    @abstractmethod
+    def get_config(self, goal_objects):
+        pass
 
     @staticmethod
     def find_all_valid_objects(constraint_list, objects):
@@ -46,6 +115,17 @@ class Goal(ABC):
         return valid_objects
 
 
+class NullGoal(Goal):
+    def __init__(self):
+        super(NullGoal, self).__init__()
+
+    def compute_objects(self, object_defs):
+        return []
+
+    def get_config(self, goal_objects):
+        return ''
+
+
 class RetrievalGoal(Goal):
     TEMPLATE = {
         'category': 'retrieval',
@@ -54,8 +134,25 @@ class RetrievalGoal(Goal):
         'task_list': ['navigate', 'localize', 'retrieve'],
     }
 
-    def get_object_constraint_lists(self):
-        return [[]]
+    def __init__(self):
+        super(RetrievalGoal, self).__init__()
+
+    def compute_objects(self, object_defs):
+        # add objects we need for the goal
+        target_def = copy.deepcopy(random.choice(object_defs))
+        performer_start = self.compute_performer_start()
+        performer_position = performer_start['position']
+        bounding_rects = []
+        target_location = calc_obj_pos(performer_position, bounding_rects, target_def)
+        if target_location is None:
+            raise GoalException('could not place target object')
+
+        target = instantiate_object(target_def, target_location)
+
+        all_objects = [target]
+        add_objects(object_defs, all_objects, bounding_rects, performer_position)
+
+        return [target], all_objects
 
     def get_config(self, objects):
         target = objects[0]
@@ -85,6 +182,9 @@ class TransferralGoal(Goal):
         'task_list': ['navigation', 'identification', 'transportation']
     }
 
+    def __init__(self):
+        super(TransferralGoal, self).__init__()
+
     def get_object_constraint_lists(self):
         return [[AttributeConstraint(list.__contains__, 'attributes', 'pickupable')], []]
 
@@ -112,7 +212,8 @@ class TransferralGoal(Goal):
             },
             'relationship': ['target_1', relationship.value, 'target_2']
         }
-        goal['description'] = f'Find and pick up the {" ".join(target1["info"])} and move it {relationship.value} the {" ".join(target2["info"])}.'
+        goal[
+            'description'] = f'Find and pick up the {" ".join(target1["info"])} and move it {relationship.value} the {" ".join(target2["info"])}.'
         return goal
 
 
@@ -121,9 +222,14 @@ GOAL_TYPES = {
 }
 
 
-def generate_goal(goal_type):
-    """Return a random class of 'goal' object from within the specified overall type"""
-    return random.choice(GOAL_TYPES[goal_type])()
+def choose_goal(goal_type):
+    """Return a random class of 'goal' object from within the specified
+overall type, or NullGoal if goal_type is None"""
+    if goal_type is None:
+        return NullGoal()
+    else:
+        return RetrievalGoal()
+#        return random.choice(GOAL_TYPES[goal_type])()
 
 
 def get_goal_types():
--- scene_generator/scene_generator.py
+++ scene_generator/scene_generator.py
@@ -8,12 +8,9 @@ import os.path
 import json
 import copy
 import random
-import uuid
-import math
 
 from materials import *
 import goals
-from separating_axis_theorem import *
 
 OUTPUT_TEMPLATE_JSON = """
 {
@@ -39,22 +36,9 @@ OUTPUT_TEMPLATE_JSON = """
 OUTPUT_TEMPLATE = json.loads(OUTPUT_TEMPLATE_JSON)
 
 # the following mins and maxes are inclusive
-MIN_PERFORMER_POSITION = -4.8
-MAX_PERFORMER_POSITION = 4.8
 MIN_SCENE_POSITION = -4.95
 MAX_SCENE_POSITION = 4.95
-POSITION_DIGITS = 1
-VALID_ROTATIONS = (0, 45, 90, 135, 180, 225, 270, 315)
 MAX_TRIES = 20
-MAX_OBJECTS = 5
-
-
-def random_position():
-    return round(random.uniform(MIN_PERFORMER_POSITION, MAX_PERFORMER_POSITION), POSITION_DIGITS)
-
-
-def random_rotation():
-    return random.choice(VALID_ROTATIONS)
 
 
 def load_object_file(object_file_name):
@@ -63,131 +47,22 @@ def load_object_file(object_file_name):
     return objects
 
 
-def dot_prod_dict(v1, v2):
-    return sum(v1[key] * v2.get(key, 0) for key in v1)
-
-
-def collision(test_rect, test_point):
-    # assuming test_rect is an array4 points in order... Clockwise or CCW does not matter
-    # points are {x,y,z}
-    #
-    # From https://math.stackexchange.com/a/190373
-    A = test_rect[0]
-    B = test_rect[1]
-    C = test_rect[2]
-
-    vectorAB = {'x': B['x'] - A['x'], 'y': B['y'] - A['y'], 'z': B['z'] - A['z']}
-    vectorBC = {'x': C['x'] - B['x'], 'y': C['y'] - B['y'], 'z': C['z'] - B['z']}
-
-    vectorAM = {'x': test_point['x'] - A['x'], 'y': test_point['y'] - A['y'], 'z': test_point['z'] - A['z']}
-    vectorBM = {'x': test_point['x'] - B['x'], 'y': test_point['y'] - B['y'], 'z': test_point['z'] - B['z']}
-
-    return (0 <= dot_prod_dict(vectorAB, vectorAM) <= dot_prod_dict(vectorAB, vectorAB)) & (
-                0 <= dot_prod_dict(vectorBC, vectorBM) <= dot_prod_dict(vectorBC, vectorBC))
-
-
-def calc_obj_coords(x, z, dx, dz, rotation):
-    """Returns an array of points that are the coordinates of the rectangle """
-    radian_amount = rotation * math.pi / 180.0
-
-    rotate_sin = math.sin(radian_amount)
-    rotate_cos = math.cos(radian_amount)
-    a = {'x': x + (dx * rotate_cos) - (dz * rotate_sin), 'y': 0, 'z': z + (dx * rotate_sin + dz * rotate_cos)}
-    b = {'x': x + (dx * rotate_cos) - (dz * rotate_sin), 'y': 0, 'z': z - (dx * rotate_sin + dz * rotate_cos)}
-    c = {'x': x - (dx * rotate_cos) + (dz * rotate_sin), 'y': 0, 'z': z - (dx * rotate_sin + dz * rotate_cos)}
-    d = {'x': x - (dx * rotate_cos) + (dz * rotate_sin), 'y': 0, 'z': z + (dx * rotate_sin + dz * rotate_cos)}
-    return [a, b, c, d]
-
-
-def calc_obj_pos(performer_position, other_rects, new_object, old_object):
-    """Returns True if we can place the object in the frame, False otherwise. """
-
-    dx = old_object['dimensions']['x']
-    dz = old_object['dimensions']['z']
-
-    tries = 0
-    while tries < MAX_TRIES:
-        rotation = random_rotation()
-        new_x = random_position()
-        new_z = random_position()
-
-        rect = calc_obj_coords(new_x, new_z, dx, dz, rotation)
-        if not collision(rect, performer_position) and (
-                len(other_rects) == 0 or any(sat_entry(rect, other_rect) for other_rect in other_rects)):
-            break
-        tries += 1
-
-    if tries < MAX_TRIES:
-        new_object['rotation'] = {'x': 0, 'y': rotation, 'z': 0}
-        new_object['position'] = {'x': new_x, 'y': old_object['position_y'], 'z': new_z}
-        other_rects.append(rect)
-        return True
-
-    return False
-
-
-def generate_file(name, objects, goal_type):
-    global OUTPUT_TEMPLATE, MAX_OBJECTS
+def generate_file(name, object_defs, goal_type):
+    global OUTPUT_TEMPLATE
     body = copy.deepcopy(OUTPUT_TEMPLATE)
     body['name'] = os.path.basename(name)
     body['ceilingMaterial'] = random.choice(CEILING_AND_WALL_MATERIALS)
     body['wallMaterial'] = random.choice(CEILING_AND_WALL_MATERIALS)
     body['floorMaterial'] = random.choice(FLOOR_MATERIALS)
-    position = body['performerStart']['position']
-    position['x'] = random_position()
-    position['y'] = 0
-    position['z'] = random_position()
-    body['performerStart']['rotation']['y'] = random_rotation()
 
-    if goal_type is not None:
-        goal_obj = goals.generate_goal(goal_type)
-        constraint_lists = goal_obj.get_object_constraint_lists()
-        min_obj_count = len(constraint_lists)
-    else:
-        min_obj_count = 1
-
-    other_rects = []
-    all_objects = []
-    object_count = random.randint(min_obj_count, MAX_OBJECTS)
-    for x in range(object_count):
-        valid_objects = objects
-        if goal_type is not None and x < len(constraint_lists):
-            constraint_list = constraint_lists[x]
-            valid_objects = goal_obj.find_all_valid_objects(constraint_list, objects)
-        selected_object = copy.deepcopy(random.choice(valid_objects))
-        shows_object = {}
-
-        if calc_obj_pos(position, other_rects, shows_object, selected_object):
-            new_object = {
-                'id': str(uuid.uuid4()),
-                'type': selected_object['type'],
-                'info': selected_object['info'],
-                'mass': selected_object['mass']
-                }
-            for attribute in selected_object['attributes']:
-                new_object[attribute] = True
-
-            shows = [shows_object]
-            new_object['shows'] = shows
-            shows_object['stepBegin'] = 0
-            shows_object['scale'] = selected_object['scale']
-            if 'salientMaterials' in selected_object:
-                salientMaterialsIndex = selected_object['salientMaterials'][0].upper() + '_MATERIALS'
-                salientMaterial = random.choice(globals().get(salientMaterialsIndex, None))
-                new_object['material'] = salientMaterial
-                new_object['info'].append(selected_object['salientMaterials'][0])
-                new_object['salientMaterials'] = selected_object['salientMaterials']
-            all_objects.append(new_object)
-
-    body['objects'] = all_objects
-    if goal_type is not None:
-        body['goal'] = goal_obj.get_config(all_objects[:min_obj_count])
+    goal_obj = goals.choose_goal(goal_type)
+    goal_obj.update_body(object_defs, body)
 
     with open(name, 'w') as out:
         json.dump(body, out, indent=2)
 
 
-def generate_one_fileset(prefix, count, objects, goal_type):
+def generate_one_fileset(prefix, count, object_defs, goal_type):
     # skip existing files
     index = 1
     dirname = os.path.dirname(prefix)
@@ -201,7 +76,7 @@ def generate_one_fileset(prefix, count, objects, goal_type):
             file_exists = os.path.exists(name)
             index += 1
 
-        generate_file(name, objects, goal_type)
+        generate_file(name, object_defs, goal_type)
         count -= 1
 
 
