f2a97fa0a8ed6b92003b028c94dbd44f7256fd0b
Dean Wetherby <dean.wetherby@gmail.com>
2020-04-17 10:05:40 -0400

    Removing just-in-case comments

--- python_api/machine_common_sense/__init__.py
+++ python_api/machine_common_sense/__init__.py
@@ -1,19 +1,3 @@
-# from machine_common_sense.mcs import MCS
-# from machine_common_sense.mcs_action import MCS_Action
-# from machine_common_sense.mcs_action_api_desc import MCS_Action_API_DESC
-# from machine_common_sense.mcs_action_keys import MCS_Action_Keys
-# from machine_common_sense.mcs_controller import MCS_Controller
-# from machine_common_sense.mcs_controller_ai2thor import MCS_Controller_AI2THOR
-# from machine_common_sense.mcs_goal import MCS_Goal
-# from machine_common_sense.mcs_goal_category import MCS_Goal_Category
-# from machine_common_sense.mcs_material import MCS_Material
-# from machine_common_sense.mcs_object import MCS_Object
-# from machine_common_sense.mcs_pose import MCS_Pose
-# from machine_common_sense.mcs_return_status import MCS_Return_Status
-# from machine_common_sense.mcs_step_output import MCS_Step_Output
-# from machine_common_sense.mcs_util import MCS_Util
-# from machine_common_sense.run_mcs_human_input import main
-
 from .mcs_action import MCS_Action
 from .mcs_action_keys import MCS_Action_Keys
 from .mcs_controller import MCS_Controller
@@ -27,4 +11,3 @@ from .mcs_return_status import MCS_Return_Status
 from .mcs_reward import MCS_Reward
 from .mcs_step_output import MCS_Step_Output
 from .mcs_util import MCS_Util
-
