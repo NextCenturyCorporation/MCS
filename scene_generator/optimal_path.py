b74044b9038be719e58954afe91181bb3f5a4570
Billy W <bwagner@pc-2152-bw.hq.nextcentury.com>
2020-04-21 12:30:59 -0400

    Fixing some code review issues

--- scene_generator/optimal_path.py
+++ scene_generator/optimal_path.py
@@ -1,8 +1,9 @@
 import logging
-
+import traceback
 
 from extremitypathfinder.extremitypathfinder import PolygonEnvironment as Environment
 from  geometry import ROOM_DIMENSIONS
+from _testcapi import traceback_print
 
 
 MIN_X = ROOM_DIMENSIONS[0][0]
@@ -14,23 +15,20 @@ MAX_Z = ROOM_DIMENSIONS[1][1]
 #target_loc will probably contain the target- we'll see
 def generatepath(source_loc, target_loc, other_rects):
     '''Boundary has to be CCW, Holes CW'''
-    boundary_coordinates = [ (MAX_X, MAX_Z),(MIN_X, MAX_Z),(MIN_X, MIN_Z), (MAX_X,MAX_Z)]
+    boundary_coordinates = [ (MAX_X, MAX_Z),(MIN_X, MAX_Z),(MIN_X, MIN_Z), (MAX_X,MIN_Z)]
     holes= []
     # This way does make me a bit nervous- as this is coding
     #need to convert to lists of points 
     for object in other_rects:            
         holes.append([(point['x'],point['z']) for point in object])
         
-        
-    environment = Environment()
-    environment.store(boundary_coordinates, holes, validate = True)
-    environment.prepare()
-    
     try:
+        environment = Environment()
+        environment.store(boundary_coordinates, holes, validate = False)
+        environment.prepare()
         path,length = environment.find_shortest_path(source_loc, target_loc)
-    except:
-        e = sys.exc_info()[0]
-        logging.error(e)
+    except Exception as e:
+        traceback.print_exc()
         logObject = {
             'boundary': boundary_coordinates,
             'holes': holes,
