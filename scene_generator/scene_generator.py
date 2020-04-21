10abe063710e1ab1b9e5b8d8131828fbe4758903
Chris Long <chris.long@nextcentury.com>
2020-04-21 13:12:23 -0400

    enhanced --goal to accept specific goal name

--- scene_generator/scene_generator.py
+++ scene_generator/scene_generator.py
@@ -79,10 +79,12 @@ def generate_file(name, goal_type):
         # PrettyJsonEncoder doesn't work with json.dump so use json.dumps here instead.
         out.write(json.dumps(body, cls=PrettyJsonEncoder, indent=2))
 
+
 def wrap_with_json_no_indent(data, prop_list):
     for prop in prop_list:
         if prop in data:
             data[prop] = PrettyJsonNoIndent(data[prop])
+
 
 def generate_fileset(prefix, count, goal_type, stop_on_error):
     # skip existing files
@@ -113,7 +115,8 @@ def main(argv):
     parser.add_argument('-c', '--count', type=int, default=1, help='How many scenes to generate [default=1]')
     parser.add_argument('--seed', type=int, default=None, help='Random number seed [default=None]')
     parser.add_argument('--goal', default=None, choices=goals.get_goal_types(),
-                        help='Generate a goal of the specified type [default is to not generate a goal]')
+                        help='Generate a goal of the specified type [default is to not generate a goal]. Lowercase '
+                             'goals are categories; capitalized goals are specific goals.')
     parser.add_argument('--stop-on-error', default=False, action='store_true',
                         help='Stop immediately if there is an error generating a file [default is print a warning but '
                              'do not stop]')
