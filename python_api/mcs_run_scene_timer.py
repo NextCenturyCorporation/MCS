36d6e4b70678dbc1ddd302754d2d062c2368709a
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2020-04-27 17:20:35 -0400

    MCS-177 Updates to timer script.

--- python_api/mcs_run_scene_timer.py
+++ python_api/mcs_run_scene_timer.py
@@ -5,6 +5,8 @@ import time
 
 from machine_common_sense.mcs import MCS
 
+DEFAULT_STEP_COUNT = 20
+
 def run_scene(file_name):
     config_data, status = MCS.load_config_json_file(file_name)
 
@@ -18,7 +20,7 @@ def run_scene(file_name):
     if 'name' not in config_data.keys():
         config_data['name'] = config_file_name[0:config_file_name.find('.')]
 
-    last_step = 20
+    last_step = DEFAULT_STEP_COUNT
     if 'goal' in config_data.keys():
         if 'last_step' in config_data['goal'].keys():
             last_step = config_data['goal']['last_step']
@@ -42,6 +44,7 @@ if __name__ == "__main__":
 
     file_list = [os.path.join(sys.argv[2], file_name) for file_name in os.listdir(sys.argv[2]) if \
             os.path.isfile(os.path.join(sys.argv[2], file_name)) and os.path.splitext(file_name)[1] == '.json']
+    file_list.sort()
 
     print(f'FOUND {len(file_list)} SCENE CONFIGURATION FILES... STARTING MCS PYTHON CONTROLLER AND UNITY APP...')
     controller = MCS.create_controller(sys.argv[1], debug='terminal')
@@ -55,8 +58,8 @@ if __name__ == "__main__":
     step_time_sum_list = []
 
     for i in range(0, len(file_list)):
-        print('========================================')
-        print('RUNNING ' + file_list[i])
+        print('================================================================================')
+        print('RUNNING FILE ' + str(i + 1) + ': ' + file_list[i])
         start = time.perf_counter()
         step_time_list = run_scene(file_list[i])
         end = time.perf_counter()
@@ -68,8 +71,8 @@ if __name__ == "__main__":
         step_time_min_list.append(min(step_time_list))
         step_time_sum_list.append(sum(step_time_list))
 
-    print('========================================')
-    print(f'RAN {len(file_list)} SCENES WITH {sum(step_time_len_list)} TOTAL STEPS IN {sum(scene_time_list):0.4f} SECONDS')
+    print('================================================================================')
+    print(f'RAN {len(file_list)} TOTAL SCENES WITH {sum(step_time_len_list)} TOTAL STEPS IN {sum(scene_time_list):0.4f} TOTAL SECONDS')
     print(f'Average single step took {statistics.mean(step_time_avg_list):0.4f} seconds')
     print(f'Longest single step took {max(step_time_max_list):0.4f} seconds')
     print(f'Shortest single step took {min(step_time_min_list):0.4f} seconds')
