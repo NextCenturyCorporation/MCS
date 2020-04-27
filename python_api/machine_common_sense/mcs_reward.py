77986b4c158b804f81d575816b02565263891ebe
Dean Wetherby <dean.wetherby@gmail.com>
2020-04-27 12:29:43 -0400

    Updating transferral reward calculation and tests

--- python_api/machine_common_sense/mcs_reward.py
+++ python_api/machine_common_sense/mcs_reward.py
@@ -153,7 +153,9 @@ class MCS_Reward(object):
             return GOAL_NOT_ACHIEVED
 
         # action object to goal object
-        action_id, action, goal_id = relationship
+        action_target, action, goal_target = relationship
+        action_id = action_target.get('id', None)
+        goal_id = goal_target.get('id', None)
         action = action.lower()
 
         #objects = scene_metadata['objects']
