a1b7ff5d74ed6f4abb556f9dd0b03b6e276c89b1
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2020-05-07 09:45:21 -0400

    Added cube and cylinder definitions to the intphys objects list for the scene generator.

--- scene_generator/objects_intphys_v1.py
+++ scene_generator/objects_intphys_v1.py
@@ -343,9 +343,9 @@ OBJECTS_INTPHYS = [{
         ]
     }]
 }, {
-    "type": "sphere",
-    "info": ["tiny", "ball"],
-    "mass": 0.1,
+    "type": "cube",
+    "info": ["medium", "cube"],
+    "mass": 0.75,
     "choose": [{
         "materialCategory": ["plastic"],
         "salientMaterials": ["plastic", "hollow"]
@@ -361,20 +361,526 @@ OBJECTS_INTPHYS = [{
     }],
     "attributes": ["moveable", "pickupable"],
     "dimensions": {
-        "x": 0.1,
-        "y": 0.1,
-        "z": 0.1
+        "x": 0.75,
+        "y": 0.75,
+        "z": 0.75
     },
-    "position_y": 0.05,
+    "position_y": 0.375,
     "scale": {
-        "x": 0.1,
-        "y": 0.1,
-        "z": 0.1
+        "x": 0.75,
+        "y": 0.75,
+        "z": 0.75
     },
     "intphys_options": [{
         "y": 0,
         "force": {
-            "x": 300 * 0.1,
+            "x": 300 * 0.75,
+            "y": 0,
+            "z": 0
+        },
+        "position_by_step": [
+            0.245,
+            0.484,
+            0.807,
+            1.208,
+            1.564,
+            1.794,
+            2.075,
+            2.451,
+            2.862,
+            3.121,
+            3.363,
+            3.693,
+            4.096,
+            4.44,
+            4.669,
+            4.956,
+            5.336,
+            5.748,
+            5.998,
+            6.241,
+            6.574,
+            6.978,
+            7.315,
+            7.545,
+            7.839,
+            8.224,
+            8.636,
+            8.877,
+            9.121,
+            9.455,
+            9.859,
+            10.195,
+            10.424,
+            10.719,
+            11.104
+        ]
+    }, {
+        "y": 0,
+        "force": {
+            "x": 450 * 0.75,
+            "y": 0,
+            "z": 0
+        },
+        "position_by_step": [
+            0.46,
+            0.861,
+            1.466,
+            2.1,
+            2.584,
+            2.991,
+            3.605,
+            4.24,
+            4.727,
+            5.134,
+            5.741,
+            6.375,
+            6.861,
+            7.272,
+            7.881,
+            8.514,
+            8.995,
+            9.405,
+            10.021,
+            10.654,
+            11.136
+        ]
+    }, {
+        "y": 0,
+        "force": {
+            "x": 600 * 0.75,
+            "y": 0,
+            "z": 0
+        },
+        "position_by_step": [
+            0.684,
+            1.31,
+            2.141,
+            2.999,
+            3.709,
+            4.327,
+            5.177,
+            6.035,
+            6.742,
+            7.36,
+            8.212,
+            9.027,
+            10.402,
+            11.245
+        ]
+    }]
+}, {
+    "type": "cube",
+    "info": ["small", "cube"],
+    "mass": 0.5,
+    "choose": [{
+        "materialCategory": ["plastic"],
+        "salientMaterials": ["plastic", "hollow"]
+    }, {
+        "materialCategory": ["rubber"],
+        "salientMaterials": ["rubber"]
+    }, {
+        "materialCategory": ["wood"],
+        "salientMaterials": ["wood"]
+    }, {
+        "materialCategory": ["metal"],
+        "salientMaterials": ["metal"]
+    }],
+    "attributes": ["moveable", "pickupable"],
+    "dimensions": {
+        "x": 0.5,
+        "y": 0.5,
+        "z": 0.5
+    },
+    "position_y": 0.25,
+    "scale": {
+        "x": 0.5,
+        "y": 0.5,
+        "z": 0.5
+    },
+    "intphys_options": [{
+        "y": 0,
+        "force": {
+            "x": 300 * 0.5,
+            "y": 0,
+            "z": 0
+        },
+        "position_by_step": [
+            0.247,
+            0.511,
+            0.909,
+            1.205,
+            1.461,
+            1.848,
+            2.169,
+            2.416,
+            2.789,
+            3.133,
+            3.373,
+            3.73,
+            4.092,
+            4.33,
+            4.668,
+            5.048,
+            5.285,
+            5.593,
+            6.003,
+            6.248,
+            6.525,
+            6.93,
+            7.206,
+            7.468,
+            7.864,
+            8.161,
+            8.419,
+            8.807,
+            9.123,
+            9.373,
+            9.751,
+            10.086,
+            10.329,
+            10.695
+        ]
+    }, {
+        "y": 0,
+        "force": {
+            "x": 450 * 0.5,
+            "y": 0,
+            "z": 0
+        },
+        "position_by_step": [
+            0.454,
+            0.898,
+            1.558,
+            2.059,
+            2.5,
+            3.155,
+            3.676,
+            4.099,
+            4.751,
+            5.291,
+            5.713,
+            6.358,
+            6.888,
+            7.308,
+            7.958,
+            8.499,
+            8.923,
+            9.567,
+            10.093,
+            10.515,
+            11.166
+        ]
+    }, {
+        "y": 0,
+        "force": {
+            "x": 600 * 0.5,
+            "y": 0,
+            "z": 0
+        },
+        "position_by_step": [
+            0.679,
+            1.351,
+            2.236,
+            2.96,
+            3.618,
+            4.5,
+            5.239,
+            5.891,
+            6.772,
+            7.526,
+            8.169,
+            9.047,
+            9.804,
+            10.45,
+            11.325
+        ]
+    }]
+}, {
+    "type": "cube",
+    "info": ["tiny", "cube"],
+    "mass": 0.25,
+    "choose": [{
+        "materialCategory": ["plastic"],
+        "salientMaterials": ["plastic", "hollow"]
+    }, {
+        "materialCategory": ["rubber"],
+        "salientMaterials": ["rubber"]
+    }, {
+        "materialCategory": ["wood"],
+        "salientMaterials": ["wood"]
+    }, {
+        "materialCategory": ["metal"],
+        "salientMaterials": ["metal"]
+    }],
+    "attributes": ["moveable", "pickupable"],
+    "dimensions": {
+        "x": 0.25,
+        "y": 0.25,
+        "z": 0.25
+    },
+    "position_y": 0.125,
+    "scale": {
+        "x": 0.25,
+        "y": 0.25,
+        "z": 0.25
+    },
+    "intphys_options": [{
+        "y": 0,
+        "force": {
+            "x": 300 * 0.25,
+            "y": 0,
+            "z": 0
+        },
+        "position_by_step": [
+            0.253,
+            0.616,
+            0.867,
+            1.223,
+            1.492,
+            1.841,
+            2.117,
+            2.469,
+            2.754,
+            3.093,
+            3.374,
+            3.723,
+            3.991,
+            4.351,
+            4.626,
+            4.976,
+            5.26,
+            5.602,
+            5.886,
+            6.23,
+            6.514,
+            6.864,
+            7.147,
+            7.492,
+            7.774,
+            8.125,
+            8.414,
+            8.747,
+            9.037,
+            9.378,
+            9.671,
+            10.005,
+            10.293,
+            10.643
+        ]
+    }, {
+        "y": 0,
+        "force": {
+            "x": 450 * 0.25,
+            "y": 0,
+            "z": 0
+        },
+        "position_by_step": [
+            0.469,
+            1.064,
+            1.536,
+            2.121,
+            2.613,
+            3.16,
+            3.642,
+            4.244,
+            4.751,
+            5.284,
+            5.814,
+            6.349,
+            6.892,
+            7.413,
+            7.954,
+            8.485,
+            9.022,
+            9.552,
+            10.082,
+            10.621
+        ]
+    }, {
+        "y": 0,
+        "force": {
+            "x": 600 * 0.25,
+            "y": 0,
+            "z": 0
+        },
+        "position_by_step": [
+            0.694,
+            1.514,
+            2.211,
+            3.019,
+            3.738,
+            4.509,
+            5.222,
+            6.042,
+            6.775,
+            7.533,
+            8.288,
+            9.05,
+            9.798,
+            10.571
+        ]
+    }]
+}, {
+    "type": "cylinder",
+    "info": ["medium", "cylinder"],
+    "mass": 0.75,
+    "choose": [{
+        "materialCategory": ["plastic"],
+        "salientMaterials": ["plastic", "hollow"]
+    }, {
+        "materialCategory": ["rubber"],
+        "salientMaterials": ["rubber"]
+    }, {
+        "materialCategory": ["wood"],
+        "salientMaterials": ["wood"]
+    }, {
+        "materialCategory": ["metal"],
+        "salientMaterials": ["metal"]
+    }],
+    "attributes": ["moveable", "pickupable"],
+    "dimensions": {
+        "x": 0.75,
+        "y": 0.75,
+        "z": 0.75
+    },
+    "position_y": 0.375,
+    "rotation": {
+        "x": 90,
+        "y": 0,
+        "z": 0
+    },
+    "scale": {
+        "x": 0.75,
+        "y": 0.75,
+        "z": 0.75
+    },
+    "intphys_options": [{
+        "y": 0,
+        "force": {
+            "x": 300 * 0.75,
+            "y": 0,
+            "z": 0
+        },
+        "position_by_step": [
+            0.38,
+            0.759,
+            1.139,
+            1.518,
+            1.898,
+            2.278,
+            2.657,
+            3.037,
+            3.416,
+            3.796,
+            4.176,
+            4.555,
+            4.935,
+            5.314,
+            5.694,
+            6.074,
+            6.453,
+            6.833,
+            7.213,
+            7.592,
+            7.972,
+            8.351,
+            8.731,
+            9.111,
+            9.49,
+            9.87,
+            10.249,
+            10.629,
+            11.009
+        ]
+    }, {
+        "y": 0,
+        "force": {
+            "x": 450 * 0.75,
+            "y": 0,
+            "z": 0
+        },
+        "position_by_step": [
+            0.604,
+            1.209,
+            1.813,
+            2.417,
+            3.022,
+            3.626,
+            4.231,
+            4.835,
+            5.439,
+            6.044,
+            6.648,
+            7.252,
+            7.857,
+            8.461,
+            9.066,
+            9.67,
+            10.274,
+            10.879
+        ]
+    }, {
+        "y": 0,
+        "force": {
+            "x": 600 * 0.75,
+            "y": 0,
+            "z": 0
+        },
+        "position_by_step": [
+            0.829,
+            1.659,
+            2.488,
+            3.317,
+            4.147,
+            4.976,
+            5.806,
+            6.635,
+            7.464,
+            8.294,
+            9.123,
+            9.952,
+            10.782
+        ]
+    }]
+}, {
+    "type": "cylinder",
+    "info": ["small", "cylinder"],
+    "mass": 0.5,
+    "choose": [{
+        "materialCategory": ["plastic"],
+        "salientMaterials": ["plastic", "hollow"]
+    }, {
+        "materialCategory": ["rubber"],
+        "salientMaterials": ["rubber"]
+    }, {
+        "materialCategory": ["wood"],
+        "salientMaterials": ["wood"]
+    }, {
+        "materialCategory": ["metal"],
+        "salientMaterials": ["metal"]
+    }],
+    "attributes": ["moveable", "pickupable"],
+    "dimensions": {
+        "x": 0.5,
+        "y": 0.5,
+        "z": 0.5
+    },
+    "position_y": 0.25,
+    "rotation": {
+        "x": 90,
+        "y": 0,
+        "z": 0
+    },
+    "scale": {
+        "x": 0.5,
+        "y": 0.5,
+        "z": 0.5
+    },
+    "intphys_options": [{
+        "y": 0,
+        "force": {
+            "x": 300 * 0.5,
             "y": 0,
             "z": 0
         },
@@ -405,12 +911,13 @@ OBJECTS_INTPHYS = [{
             9.105,
             9.484,
             9.864,
-            10.243
+            10.243,
+            10.622
         ]
     }, {
         "y": 0,
         "force": {
-            "x": 450 * 0.1,
+            "x": 450 * 0.5,
             "y": 0,
             "z": 0
         },
@@ -431,12 +938,134 @@ OBJECTS_INTPHYS = [{
             8.461,
             9.066,
             9.67,
-            10.274
+            10.274,
+            10.879
         ]
     }, {
         "y": 0,
         "force": {
-            "x": 600 * 0.1,
+            "x": 600 * 0.5,
+            "y": 0,
+            "z": 0
+        },
+        "position_by_step": [
+            0.829,
+            1.659,
+            2.488,
+            3.317,
+            4.147,
+            4.976,
+            5.806,
+            6.635,
+            7.464,
+            8.294,
+            9.123,
+            9.952,
+            10.782
+        ]
+    }]
+}, {
+    "type": "cylinder",
+    "info": ["tiny", "cylinder"],
+    "mass": 0.25,
+    "choose": [{
+        "materialCategory": ["plastic"],
+        "salientMaterials": ["plastic", "hollow"]
+    }, {
+        "materialCategory": ["rubber"],
+        "salientMaterials": ["rubber"]
+    }, {
+        "materialCategory": ["wood"],
+        "salientMaterials": ["wood"]
+    }, {
+        "materialCategory": ["metal"],
+        "salientMaterials": ["metal"]
+    }],
+    "attributes": ["moveable", "pickupable"],
+    "dimensions": {
+        "x": 0.25,
+        "y": 0.25,
+        "z": 0.25
+    },
+    "position_y": 0.125,
+    "rotation": {
+        "x": 90,
+        "y": 0,
+        "z": 0
+    },
+    "scale": {
+        "x": 0.25,
+        "y": 0.125,
+        "z": 0.25
+    },
+    "intphys_options": [{
+        "y": 0,
+        "force": {
+            "x": 300 * 0.25,
+            "y": 0,
+            "z": 0
+        },
+        "position_by_step": [
+            0.379,
+            0.759,
+            1.138,
+            1.517,
+            1.897,
+            2.276,
+            2.656,
+            3.035,
+            3.414,
+            3.794,
+            4.173,
+            4.552,
+            4.932,
+            5.311,
+            5.691,
+            6.07,
+            6.449,
+            6.829,
+            7.208,
+            7.587,
+            7.967,
+            8.346,
+            8.725,
+            9.105,
+            9.484,
+            9.864,
+            10.243,
+            10.622
+        ]
+    }, {
+        "y": 0,
+        "force": {
+            "x": 450 * 0.25,
+            "y": 0,
+            "z": 0
+        },
+        "position_by_step": [
+            0.604,
+            1.209,
+            1.813,
+            2.417,
+            3.022,
+            3.626,
+            4.231,
+            4.835,
+            5.439,
+            6.044,
+            6.648,
+            7.252,
+            7.857,
+            8.461,
+            9.066,
+            9.67,
+            10.274,
+            10.879
+        ]
+    }, {
+        "y": 0,
+        "force": {
+            "x": 600 * 0.25,
             "y": 0,
             "z": 0
         },
@@ -457,4 +1086,3 @@ OBJECTS_INTPHYS = [{
         ]
     }]
 }]
-
