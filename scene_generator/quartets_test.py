3ee15a9051fed54e483659cb58863991591b066f
Chris Long <chris.long@nextcentury.com>
2020-05-20 11:13:35 -0400

    updated unit tests

--- scene_generator/quartets_test.py
+++ scene_generator/quartets_test.py
@@ -18,14 +18,15 @@ def test_ShapeConstancyQuartet_get_scene_2():
     template = {'wallMaterial': 'dummy'}
     quartet = ShapeConstancyQuartet(template, False)
     scene = quartet.get_scene(2)
+    a = scene['objects'][0]
+    b = scene['objects'][-1]
+    assert a['hides'][0]['stepBegin'] == b['shows'][0]['stepBegin']
     if quartet._goal._object_creator == intphys_goals.IntPhysGoal._get_objects_and_occluders_moving_across:
-        a = scene['objects'][0]
-        b = scene['objects'][-1]
-        assert a['hides'][0]['stepBegin'] == b['shows'][0]['stepBegin']
         assert a['forces'] == b['forces']
     else:
-        # TODO: in MCS-131
-        pass
+        assert a['shows'][0]['stepBegin'] >= 8
+        assert b['shows'][0]['position']['x'] == a['shows'][0]['position']['x']
+        assert b['shows'][0]['position']['y'] == a['intphys_option']['position_y']
 
 
 def test_ShapeConstancyQuartet_get_scene_3():
@@ -33,11 +34,11 @@ def test_ShapeConstancyQuartet_get_scene_3():
     template = {'wallMaterial': 'dummy'}
     quartet = ShapeConstancyQuartet(template, False)
     scene = quartet.get_scene(3)
+    a = scene['objects'][0]
+    b = scene['objects'][-1]
+    assert b['hides'][0]['stepBegin'] == a['shows'][0]['stepBegin']
     if quartet._goal._object_creator == intphys_goals.IntPhysGoal._get_objects_and_occluders_moving_across:
-        a = scene['objects'][0]
-        b = scene['objects'][-1]
-        assert b['hides'][0]['stepBegin'] == a['shows'][0]['stepBegin']
         assert a['forces'] == b['forces']
     else:
-        # TODO: in MCS-131
-        pass
+        assert a['shows'][0]['stepBegin'] >= 8
+        assert a['shows'][0]['position']['y'] == a['intphys_option']['position_y']
