5c3a6d2f05e1db52c9f0336b460c1099a18d57d8
Chris Long <chris.long@nextcentury.com>
2020-06-04 16:29:56 -0400

    functions for creating similar objects

--- scene_generator/util.py
+++ scene_generator/util.py
@@ -61,6 +61,7 @@ def instantiate_object(object_def: Dict[str, Any],
     object_location['scale'] = object_def['scale']
     colors = set()
     if materials_list is None and 'materialCategory' in object_def:
+        new_object['materialCategory'] = object_def['materialCategory']
         materials_list = [random.choice(getattr(materials, name.upper() + '_MATERIALS')) for name in
                           object_def['materialCategory']]
     if materials_list is not None:
@@ -92,3 +93,79 @@ def instantiate_object(object_def: Dict[str, Any],
     info.append(' '.join(info))
     new_object['info'] = info
     return new_object
+
+
+def create_similar(obj: Dict[str, Any]) -> Dict[str, Any]:
+    choice = random.randint(1, 3)
+    if choice == 1:
+        return create_with_new_type(obj)
+    elif choice == 2:
+        return create_with_new_material(obj)
+    else:
+        return create_with_new_scale(obj)
+
+
+def test_same_and_different(a: Dict[str, Any], b: Dict[str, Any], same: Iterable[str], different: Iterable[str]) -> bool:
+    """Return true iff for all properties in same that are in a, they
+    exist in b and have the same value, and for all properties in
+    different that are in a, they exist in b and are different.
+    """
+    same_ok = True
+    for prop in same:
+        if prop in a:
+            if prop not in b or a[prop] != b[prop]:
+                same_ok = False
+                break
+    if not same_ok:
+        return False
+    diff_ok = True
+    for prop in different:
+        if prop in a:
+            if prop not in b or a[prop] == b[prop]:
+                diff_ok = False
+                break
+    return diff_ok
+
+    
+def get_similar_defs(obj: Dict[str, Any], same: Iterable[str],
+                     different: Iterable[str]) -> Dict[str, Any]:
+    """Return object definitions similar to obj: where properties from
+    same are identical and from different are different.
+    """
+    valid_defs = []
+    for obj_def in objects.get_all_object_defs():
+        if test_same_and_different(obj_def, obj, same, different):
+            # now test the choose part if it's there
+            if 'choose' in obj_def:
+                valid_choices = [choice for choice in obj_def['choose']
+                                 if test_same_and_different(choice, obj, same, different)]
+                if len(valid_choices) != 0:
+                    new_def = copy.deepcopy(obj_def)
+                    new_def['choose'] = valid_choices
+                    valid_defs.append(new_def)
+            else:
+                valid_defs.append(obj_def)
+    return valid_defs
+
+
+def get_def_with_new_type(obj: Dict[str, Any]) -> Dict[str, Any]:
+    valid_defs = get_similar_defs(obj, ('materialCategory',
+                                        'similarityScale'),
+                                  ('type'))
+    obj_def = random.choice(valid_defs)
+    return obj_def
+
+def get_def_with_new_material(obj: Dict[str, Any]) -> Dict[str, Any]:
+    valid_defs = get_similar_defs(obj, ('type',
+                                        'similarityScale'),
+                                  ('materialCategory'))
+    obj_def = random.choice(valid_defs)
+    return obj_def
+
+
+def create_with_new_scale(obj: Dict[str, Any]) -> Dict[str, Any]:
+    valid_defs = get_similar_defs(obj, ('type',
+                                        'materialCategory'),
+                                  ('similarityScale'))
+    obj_def = random.choice(valid_defs)
+    return obj_def
