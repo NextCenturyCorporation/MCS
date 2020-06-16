a68aa37cc951150b1c92efc4b2150485804ec47c
Arash Behpour <abehpour@caci2006212-ab.hq.nextcentury.com>
2020-06-16 16:57:40 -0400

    added seed to controller. Worked on user input.

--- python_api/machine_common_sense/run_mcs_human_input.py
+++ python_api/machine_common_sense/run_mcs_human_input.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 
 from machine_common_sense.mcs import MCS
 from machine_common_sense.mcs_action import MCS_Action
@@ -112,11 +113,24 @@ def run_scene(controller, config_data):
 
     sys.exit()
 
-def main():
-    if len(sys.argv) < 3:
-        print('Usage: python run_mcs_human_input.py <mcs_unity_build_file> <mcs_config_json_file> <debug_files> <enable_noise>')
-        sys.exit()
+def main(): #(argv):
+    """
+    parser = argparse.ArgumentParser(description='Run MCS')
+    parser.add_argument('mcs_unity_build_file', required=True, help='Path to MCS unity build file')
+    parser.add_argument('mcs_config_json_file', required=True, help='MCS JSON scene configuration file to load')
 
+    parser.add_argument('-d','--debug', default=True, nargs=1, help='True or False on whether to debug files [default=True]')
+    parser.add_argument('-n','--noise', default=False, nargs=1, help='True or False on whether to enable noise in MCS [default=True]')
+    parser.add_argument('-s','--seed', type=int, default=None, nargs=1, help='Seed(integer) for the random number generator [default=None]')
+    args = parser.parse_args(argv[1:])
+    print(args)
+    exit()
+    """
+    #"""
+    if len(sys.argv) < 3:
+        print('Usage: python run_mcs_human_input.py <mcs_unity_build_file> <mcs_config_json_file> <debug_files> <enable_noise> <seed_value>')
+        sys.exit()
+    
     config_data, status = MCS.load_config_json_file(sys.argv[2])
 
     if status is not None:
@@ -131,7 +145,28 @@ def main():
     if len(sys.argv) >= 5:
         enable_noise = sys.argv[4].lower() == 'true'
 
-    controller = MCS.create_controller(sys.argv[1], debug=debug, enable_noise=enable_noise)
+    seed_val = None
+    if len(sys.argv) >= 6:
+        seed_val = sys.argv[5]
+
+        if not seed_val.isnumeric():
+            print(seed_val, ' is not a numeric number')
+            exit()
+        seed_val = int(seed_val)
+    #"""
+    """
+    config_data, status = MCS.load_config_json_file(args.mcs_config_json_file)
+
+    if status is not None:
+        print(status)
+        exit()
+
+    debug = args.debug
+    enable_noise = args.noise
+    seed_val = args.seed
+    """
+    
+    controller = MCS.create_controller(sys.argv[1], debug=debug, enable_noise=enable_noise, seed=seed_val)
 
     config_file_path = sys.argv[2]
     config_file_name = config_file_path[config_file_path.rfind('/')+1:]
@@ -142,5 +177,6 @@ def main():
     run_scene(controller, config_data)
 
 if __name__ == "__main__":
+    #main(sys.argv)
     main()
 
