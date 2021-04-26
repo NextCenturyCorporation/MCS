8eb565315207a9bae551ab8a15fdc853e5b4ae90
deanwetherby <dean.wetherby@ieee.org>
2021-04-26 17:19:11 -0400

    Adding version and autosummary to doc config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,17 +14,18 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
+from machine_common_sense import _version
 
 # -- Project information -----------------------------------------------------
 
 project = 'Machine Common Sense'
-copyright = '2020, CACI'
+copyright = '2021, CACI'
 author = 'CACI'
 
 # The short X.Y version
-version = ''
+version = _version.__version__
 # The full version, including alpha/beta/rc tags
-release = ''
+# release = ''
 
 # -- General configuration ---------------------------------------------------
 
@@ -36,7 +37,8 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
+    # 'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
     'sphinx.ext.githubpages',
     'sphinx.ext.intersphinx',
     'sphinx.ext.ifconfig',
@@ -78,6 +80,7 @@ pygments_style = 'sphinx'
 html_theme = 'classic'
 
 napoleon_use_ivar = True
+autogenerate_summary = True
 
 
 def setup(app):
