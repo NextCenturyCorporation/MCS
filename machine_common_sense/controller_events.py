e7c69a9260719143bd5fbd62a6a4aad6af4ffcfb
Kyle Drumm <kyle.drumm@caci.com>
2021-05-03 10:24:57 -0400

    updated EventType enum based on PR comments

--- machine_common_sense/controller_events.py
+++ machine_common_sense/controller_events.py
@@ -4,11 +4,11 @@ import enum
 
 
 class EventType(enum.Enum):
-    ON_INIT = 1
-    ON_START_SCENE = 2
-    ON_BEFORE_STEP = 3
-    ON_AFTER_STEP = 4
-    ON_END_SCENE = 5
+    ON_INIT = enum.auto()
+    ON_START_SCENE = enum.auto()
+    ON_BEFORE_STEP = enum.auto()
+    ON_AFTER_STEP = enum.auto()
+    ON_END_SCENE = enum.auto()
 
 
 class ControllerEventPayload():
