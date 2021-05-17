d7c554d2d88868a6749b15286711379a00a62459
Kyle Drumm <kyle.drumm@caci.com>
2021-05-17 08:02:06 -0400

    updated add_subscribers test

--- tests/test_mcs.py
+++ tests/test_mcs.py
@@ -22,12 +22,16 @@ class TestMCS(unittest.TestCase):
         def return_false():
             return False
 
+        cfg.is_history_enabled = return_false
         cfg.is_save_debug_images = return_false
         cfg.is_depth_maps_enabled = return_false
         cfg.is_object_masks_enabled = return_false
         cfg.is_video_enabled = return_false
         mcs._add_subscribers(ctrl, cfg)
         self.assertEqual(len(ctrl._subscribers), 2)
+        classes = (mcs.ControllerLogger, mcs.HistoryEventHandler)
+        self.assertIsInstance(ctrl._subscribers[0], classes)
+        self.assertIsInstance(ctrl._subscribers[1], classes)
 
         ctrl._subscribers.clear()
         cfg.is_save_debug_images = return_true
@@ -36,6 +40,10 @@ class TestMCS(unittest.TestCase):
         cfg.is_video_enabled = return_false
         mcs._add_subscribers(ctrl, cfg)
         self.assertEqual(len(ctrl._subscribers), 4)
+        self.assertIsInstance(ctrl._subscribers[0], mcs.DepthImageEventHandler)
+        self.assertIsInstance(ctrl._subscribers[1], mcs.SceneImageEventHandler)
+        self.assertIsInstance(ctrl._subscribers[2], mcs.ControllerLogger)
+        self.assertIsInstance(ctrl._subscribers[3], mcs.HistoryEventHandler)
 
         ctrl._subscribers.clear()
         cfg.is_save_debug_images = return_false
@@ -44,6 +52,15 @@ class TestMCS(unittest.TestCase):
         cfg.is_video_enabled = return_true
         mcs._add_subscribers(ctrl, cfg)
         self.assertEqual(len(ctrl._subscribers), 5)
+        self.assertIsInstance(ctrl._subscribers[0], mcs.ImageVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[1],
+            mcs.TopdownVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[2],
+            mcs.HeatmapVideoEventHandler)
+        self.assertIsInstance(ctrl._subscribers[3], mcs.ControllerLogger)
+        self.assertIsInstance(ctrl._subscribers[4], mcs.HistoryEventHandler)
 
         ctrl._subscribers.clear()
         cfg.is_save_debug_images = return_false
@@ -52,6 +69,18 @@ class TestMCS(unittest.TestCase):
         cfg.is_video_enabled = return_true
         mcs._add_subscribers(ctrl, cfg)
         self.assertEqual(len(ctrl._subscribers), 6)
+        self.assertIsInstance(ctrl._subscribers[0], mcs.ImageVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[1],
+            mcs.TopdownVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[2],
+            mcs.HeatmapVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[3],
+            mcs.SegmentationVideoEventHandler)
+        self.assertIsInstance(ctrl._subscribers[4], mcs.ControllerLogger)
+        self.assertIsInstance(ctrl._subscribers[5], mcs.HistoryEventHandler)
 
         ctrl._subscribers.clear()
         cfg.is_save_debug_images = return_true
@@ -60,6 +89,10 @@ class TestMCS(unittest.TestCase):
         cfg.is_video_enabled = return_false
         mcs._add_subscribers(ctrl, cfg)
         self.assertEqual(len(ctrl._subscribers), 4)
+        self.assertIsInstance(ctrl._subscribers[0], mcs.DepthImageEventHandler)
+        self.assertIsInstance(ctrl._subscribers[1], mcs.SceneImageEventHandler)
+        self.assertIsInstance(ctrl._subscribers[2], mcs.ControllerLogger)
+        self.assertIsInstance(ctrl._subscribers[3], mcs.HistoryEventHandler)
 
         ctrl._subscribers.clear()
         cfg.is_save_debug_images = return_true
@@ -68,6 +101,13 @@ class TestMCS(unittest.TestCase):
         cfg.is_video_enabled = return_false
         mcs._add_subscribers(ctrl, cfg)
         self.assertEqual(len(ctrl._subscribers), 5)
+        self.assertIsInstance(ctrl._subscribers[0], mcs.DepthImageEventHandler)
+        self.assertIsInstance(ctrl._subscribers[1], mcs.SceneImageEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[2],
+            mcs.ObjectMaskImageEventHandler)
+        self.assertIsInstance(ctrl._subscribers[3], mcs.ControllerLogger)
+        self.assertIsInstance(ctrl._subscribers[4], mcs.HistoryEventHandler)
 
         ctrl._subscribers.clear()
         cfg.is_save_debug_images = return_false
@@ -76,6 +116,21 @@ class TestMCS(unittest.TestCase):
         cfg.is_video_enabled = return_true
         mcs._add_subscribers(ctrl, cfg)
         self.assertEqual(len(ctrl._subscribers), 7)
+        self.assertIsInstance(ctrl._subscribers[0], mcs.ImageVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[1],
+            mcs.TopdownVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[2],
+            mcs.HeatmapVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[3],
+            mcs.DepthVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[4],
+            mcs.SegmentationVideoEventHandler)
+        self.assertIsInstance(ctrl._subscribers[5], mcs.ControllerLogger)
+        self.assertIsInstance(ctrl._subscribers[6], mcs.HistoryEventHandler)
 
         ctrl._subscribers.clear()
         cfg.is_save_debug_images = return_true
@@ -84,6 +139,28 @@ class TestMCS(unittest.TestCase):
         cfg.is_video_enabled = return_true
         mcs._add_subscribers(ctrl, cfg)
         self.assertEqual(len(ctrl._subscribers), 10)
+        self.assertIsInstance(ctrl._subscribers[0], mcs.DepthImageEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[1],
+            mcs.SceneImageEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[2],
+            mcs.ObjectMaskImageEventHandler)
+        self.assertIsInstance(ctrl._subscribers[3], mcs.ImageVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[4],
+            mcs.TopdownVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[5],
+            mcs.HeatmapVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[6],
+            mcs.DepthVideoEventHandler)
+        self.assertIsInstance(
+            ctrl._subscribers[7],
+            mcs.SegmentationVideoEventHandler)
+        self.assertIsInstance(ctrl._subscribers[8], mcs.ControllerLogger)
+        self.assertIsInstance(ctrl._subscribers[9], mcs.HistoryEventHandler)
 
     def test_load_scene_file_json(self):
         actual, status = mcs.load_scene_json_file("tests/test_scene.json")
