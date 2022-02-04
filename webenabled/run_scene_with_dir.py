96df03655acbf93e1d4ca6b2454f770eba81e170
clark dorman <clarkus_dormanus@hotmail.com>
2022-02-04 08:13:13 -0500

    Load scene by command, not hard-coded, in run_scene_with_dir. WIP, still need to send command

--- webenabled/run_scene_with_dir.py
+++ webenabled/run_scene_with_dir.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import time
 import uuid
+from os.path import exists
 
 import machine_common_sense as mcs
 from machine_common_sense import StepMetadata
@@ -11,68 +12,16 @@ from watchdog.observers import Observer
 
 class RunSceneWithDir:
 
-    def __init__(self, scene_file, command_in_dir, image_out_dir):
-        self.scene_file = scene_file
+    def __init__(self, command_in_dir, image_out_dir):
+        self.scene_file = None
+        self.controller = None
         self.command_in_dir = command_in_dir
         self.image_out_dir = image_out_dir
-        self.controller = mcs.create_controller(config_file_or_dict='./config_level1.ini')
-
-    def step_and_save(self, command):
-        output: StepMetadata = self.controller.step(command)
-        try:
-            img_list = output.image_list
-            if len(img_list) > 0:
-                img_path = self.image_out_dir + str(uuid.uuid4()) + ".png"
-                print(f"command: {command}.  Saved image to: {img_path}")
-                img = img_list[0]
-                img.save(img_path)
-                return img_path
-            else:
-                pass
-                # print("Image list is empty!!")
-        except Exception as e:
-            print(f"didnt work {e}")
-
-    def load_command_file(self, command_text_file):
-        try:
-            with open(command_text_file, 'r') as command_file:
-                commands = [line.strip() for line in command_file]
-        except:
-            print(f"Failed to open {command_text_file}")
-
-        for command in commands:
-            self.step_and_save(command)
-
-    def on_created(self, event):
-        # print(f"{event.src_path} created!")
-        self.load_command_file(event.src_path)
-        os.unlink(event.src_path)
-
-        # TODO:  Write out results and save
-
-    def on_deleted(self, event):
-        # print(f"deleted {event.src_path}!")
-        pass
-
-    def on_modified(self, event):
-        print(f"{event.src_path} has been modified")
-        self.load_command_file(event.src_path)
-        os.unlink(event.src_path)
-
-    def on_moved(self, event):
-        # print(f"moved {event.src_path} to {event.dest_path}")
-        pass
 
     def run_loop(self):
         print(f"Starting controller.  Watching files at {command_dir}.  Writing images to {image_dir}")
 
-        scene_data, status = mcs.load_scene_json_file(self.scene_file)
-        if status is not None:
-            print(status)
-            exit()
-
-        self.controller.start_scene(scene_data)
-        self.step_and_save('Pass')
+        self.controller = mcs.create_controller(config_file_or_dict='./config_level1.ini')
 
         # print("Staring to listen for commands")
         patterns = ["command_*.txt"]
@@ -98,12 +47,92 @@ class RunSceneWithDir:
 
         self.controller.end_scene()
 
+    def load_command_file(self, command_text_file):
+        try:
+            with open(command_text_file, 'r') as command_file:
+                commands = [line.strip() for line in command_file]
+        except:
+            print(f"Failed to open {command_text_file}")
+            return
+
+        for command in commands:
+            # Ignore blank lines
+            if command == "":
+                continue
+
+            # Handle file names (new scene file) by loading
+            if command.endswith(".json"):
+                self.scene_file = command
+                self.load_scene()
+            else:
+                # Otherwise, assume that it is a valid action
+                self.step_and_save(command)
+
+    def load_scene(self):
+
+        if not exists(self.scene_file):
+            print(f"File {self.scene_file} does not exist")
+            return
+
+        try:
+            scene_data, status = mcs.load_scene_json_file(self.scene_file)
+            if status is not None:
+                raise ValueError("Unable to read in {self.scene_file}: {status}")
+
+            self.controller.end_scene()
+            output: StepMetadata = self.controller.start_scene(scene_data)
+            self.save_output(output)
+        except Exception as e:
+            print(f"Loading scene file failed: {e}")
+
+        # TODO:  Decide if we need to do this.  It may allows us to get an image
+        # self.step_and_save('Pass')
+
+    def step_and_save(self, command):
+        output: StepMetadata = self.controller.step(command)
+        if output is not None:
+            self.save_output(output)
+
+    def save_output(self, output: StepMetadata):
+        try:
+            img_list = output.image_list
+            if len(img_list) > 0:
+                img_path = self.image_out_dir + str(uuid.uuid4()) + ".png"
+                print(f"Saved image to: {img_path}")
+                img = img_list[0]
+                img.save(img_path)
+                return img_path
+            else:
+                pass
+                # print("Image list is empty!!")
+        except Exception as e:
+            print(f"saving image didnt work: {e}")
+
+    # ----------------------------------
+    # Watchdog functions
+    # ----------------------------------
+    def on_created(self, event):
+        # print(f"{event.src_path} created!")
+        self.load_command_file(event.src_path)
+        os.unlink(event.src_path)
+
+    def on_modified(self, event):
+        print(f"{event.src_path} has been modified")
+        self.load_command_file(event.src_path)
+        os.unlink(event.src_path)
+
+    def on_moved(self, event):
+        pass
+
+    def on_deleted(self, event):
+        pass
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Run MCS')
-    parser.add_argument(
-        'mcs_scene_json_file',
-        help='MCS JSON scene configuration file to load')
+    # parser.add_argument(
+    #     'mcs_scene_json_file',
+    #     help='MCS JSON scene configuration file to load')
     parser.add_argument(
         '--mcs_command_in_dir',
         help='MCS directory that commands will appear in')
@@ -115,9 +144,9 @@ def parse_args():
 
 if __name__ == "__main__":
     args = parse_args()
-    scene_file = args.mcs_scene_json_file
+    # scene_file = args.mcs_scene_json_file
     command_dir = args.mcs_command_in_dir
     image_dir = args.mcs_image_out_dir
 
-    runScene = RunSceneWithDir(scene_file, command_dir, image_dir)
+    runScene = RunSceneWithDir(command_dir, image_dir)
     runScene.run_loop()
