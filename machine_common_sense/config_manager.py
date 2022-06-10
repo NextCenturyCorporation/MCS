bf10d7a99b63164b9c69a9e965c1593fef9264d5
alexcraneunity <alex.crane@unity3d.com>
2022-06-10 15:40:18 -0500

    Converted toggleLights to List

--- machine_common_sense/config_manager.py
+++ machine_common_sense/config_manager.py
@@ -288,7 +288,7 @@ class SceneObject(BaseModel):
     teleports: List[TeleportConfig] = None
     toggle_physics: List[SingleStepConfig] = None
     torques: List[ForceConfig] = None
-    toggleLights: SingleStepConfig = 0
+    toggleLights: List[SingleStepConfig] = None
 
     # These are deprecated, but needed for Eval 3 backwards compatibility
     can_contain_target: Optional[bool]
