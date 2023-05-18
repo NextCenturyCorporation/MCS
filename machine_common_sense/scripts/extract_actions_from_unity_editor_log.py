2a64b80ea21f92410adfec884cccaa655e3ae1fa
jaudick <jaaudick@gmail.com>
2023-05-18 15:54:54 -0400

    cleaned up new script file

--- machine_common_sense/scripts/extract_actions_from_unity_editor_log.py
+++ machine_common_sense/scripts/extract_actions_from_unity_editor_log.py
@@ -1,12 +1,14 @@
-file_path_prefix = "/Users/jacob.audick_cn/Documents/ai2thor/unity/"
-unity_editor_log_file_name = "unity_editor_console.log"
-output_file = "unity_editor_actions.txt"
-
-with open(file_path_prefix + unity_editor_log_file_name, "r") as f:
-    lines = f.readlines()
+import argparse
+import sys
 
 
-with open(output_file, "w") as f:
+def process_unity_editor_log(file_path_prefix):
+    unity_editor_log_file_name = "unity_editor_console.log"
+    output_file = "unity_editor_actions.txt"
+
+    with open(file_path_prefix + unity_editor_log_file_name, "r") as f:
+        lines = f.readlines()
+
     actions = []
     action = ""
     object_id = ""
@@ -14,11 +16,11 @@ with open(output_file, "w") as f:
         if line.startswith("MCS: Action Debug "):
             line_string = line.replace('MCS: Action Debug (', '')
             action_string_split = line_string.split(",")
-            for substring in action_string_split[:]:
-                if (substring.strip().startswith("objectImageCoords") or
-                    substring.strip().startswith("Parameters") or
-                        substring.strip().startswith("Step")):
-                    action_string_split.remove(substring)
+            action_string_split = [
+                substring for substring in action_string_split
+                if not substring.strip().startswith(
+                    ("objectImageCoords", "Parameters", "Step"))
+            ]
             action = action_string_split[0].replace("Action = ", '')
             parameters = "," + ",".join(action_string_split[1:])
             actions.append(
@@ -28,5 +30,19 @@ with open(output_file, "w") as f:
             if object_id:
                 actions[-1] = actions[-1][:-1] + f",objectId={object_id}"
 
-    for line in actions:
-        f.write(line)
+    with open(output_file, "w") as f:
+        for line in actions:
+            f.write(line)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Process Unity Editor Log')
+    parser.add_argument('prefix', type=str, help='File path prefix')
+    args = parser.parse_args(sys.argv[1:2])
+
+    file_path_prefix = args.prefix.rstrip('/') + '/'
+    process_unity_editor_log(file_path_prefix)
+
+
+if __name__ == "__main__":
+    main()
